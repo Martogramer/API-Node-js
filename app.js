b420edbfc215e0d5abaa3629b486010ea58891e8
Marton <martin.iribass@gmail.com>
2024-04-29 13:26:42 -0300

    configure mongodb prisma

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import express from "express";
-import cors from  "cors"
-import postRoute from './routes/post.route.js'
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import postRoute from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
   console.log("Escuchando");
-})
+});
 
-app.use('/api/posts', postRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+//app.use("/api/users", userRoute);
+//app.use("/api/chats", chatRoute);
+//app.use("/api/test", testRoute);
+//app.use("/api/messages", messageRoute);
